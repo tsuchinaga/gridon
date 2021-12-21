e4e4bd6c89b4e53b4b716fa0ca7c3212bc69fc4d
tsuchinaga <woocun@gmail.com>
2021-12-21 13:52:09 +0900

    リバランス時の保有ポジションの評価も資産計算ロジックを適用

--- rebalance_service.go
+++ rebalance_service.go
@@ -39,17 +39,14 @@ func (s *rebalanceService) Rebalance(strategy *Strategy) error {
 	if err != nil {
 		return err
 	}
+	price := (symbol.AskPrice + symbol.BidPrice) / 2
 
-	positions, err := s.positionStore.GetActivePositionsByStrategyCode(strategy.Code)
+	positionValue, err := s.positionValue(strategy.Code, price)
 	if err != nil {
 		return err
 	}
-	var ownedQuantity float64
-	for _, p := range positions {
-		ownedQuantity += p.OwnedQuantity // 操作可能数じゃなく、保有全数を使う
-	}
 
-	q := s.rebalanceQuantity(strategy.Cash, (symbol.AskPrice+symbol.BidPrice)/2, ownedQuantity, symbol.TradingUnit)
+	q := s.rebalanceQuantity(strategy.Cash, price, positionValue, symbol.TradingUnit)
 	switch {
 	case q < 0:
 		if err := s.orderService.ExitMarket(strategy.Code, q*-1, SortOrderLatest); err != nil {
@@ -64,15 +61,33 @@ func (s *rebalanceService) Rebalance(strategy *Strategy) error {
 	return nil
 }
 
+// positionValue - ポジションの評価額の計算
+func (s *rebalanceService) positionValue(strategyCode string, price float64) (float64, error) {
+	positions, err := s.positionStore.GetActivePositionsByStrategyCode(strategyCode)
+	if err != nil {
+		return 0, err
+	}
+
+	var val float64
+	for _, p := range positions {
+		switch p.Side {
+		case SideBuy: // 買いポジション
+			val += price * p.OwnedQuantity
+		case SideSell: // 売りポジション
+			val += (p.Price + p.Price - price) * p.OwnedQuantity
+		}
+	}
+	return val, nil
+}
+
 // rebalanceQuantity - リバランス調整数量
 // 負の値なら売り、正の値なら買い
-func (s *rebalanceService) rebalanceQuantity(cash float64, price float64, ownedQuantity float64, tradeUnit float64) float64 {
+func (s *rebalanceService) rebalanceQuantity(cash float64, price float64, positionValue float64, tradeUnit float64) float64 {
 	// ゼロ除算はできないので、必須の情報がなければ判断できないため0枚を返す
 	if price <= 0 || tradeUnit <= 0 {
 		return 0
 	}
 
-	v := ownedQuantity * price              // ポジション評価額
-	q := (cash - v) / 2 / price / tradeUnit // 現金と評価額の差 / 2 で中央値との差異を出し、その差異の中でどれだけ売買できるかを計算する
+	q := (cash - positionValue) / 2 / price / tradeUnit // 現金と評価額の差 / 2 で中央値との差異を出し、その差異の中でどれだけ売買できるかを計算する
 	return math.Round(q) * tradeUnit
 }
